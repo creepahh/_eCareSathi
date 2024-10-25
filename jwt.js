d69f336e245844097dec00167cdd3b2c22e10111
PratisthaMaharjan <pratistha.maharjan@hotmail.com>
2024-10-25 22:04:59 +0545

    caretaker authentication and api

--- jwt.js
+++ jwt.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key'; // Use a strong secret key
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+  return jwt.sign({ id: user._id, email: user.email }, SECRET_KEY, { expiresIn: '100h' });
 };
 
 const verifyToken = (token) => {
